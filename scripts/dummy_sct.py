6be8c7154fa319fdd55a12ad7f2897bc7180f21e
kist <bhu@kist.re.kr>
2022-04-26 16:10:17 +0900

    mod

--- scripts/dummy_sct.py
+++ scripts/dummy_sct.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
+import os
 import webbrowser
 import requests
 import json
@@ -17,13 +18,14 @@ sys.setdefaultencoding('utf-8')
 
 def cb_output(data):
     screen_num = int(data.data)
-
-    name_list = ["1_물다.jpg", "2_찌르다_검정.jpg ", " 3_다시듣기.jpg ", " 4_찌르다_노랑.jpg ", " 5_본실험시작.jpg ", " 6_묶다.jpg ", " 7_쫒다.jpg ", " 8_흔들다.jpg"]
-    base_url = "https://raw.githubusercontent.com/hyeonukbhin/deeptask_testbench/master/scripts/sct_screen/"
-    url = base_url + name_list[screen_num]
-    webbrowser.get(using='google-chrome').open_new(url)
-
-    pass
+    if screen_num == 999:
+        os.system("pkill -9 -ef chrome")
+    else:
+        name_list = ["1_물다.jpg", "2_찌르다_검정.jpg", "3_다시듣기.jpg", "4_찌르다_노랑.jpg", "5_본실험시작.jpg", "6_묶다.jpg", "7_쫒다.jpg", "8_흔들다.jpg"]
+        base_url = "https://raw.githubusercontent.com/hyeonukbhin/deeptask_testbench/master/scripts/sct_screen/"
+        # "https://raw.githubusercontent.com/hyeonukbhin/deeptask_testbench/master/scripts/sct_screen/1_%EB%AC%BC%EB%8B%A4.jpg"
+        url = "{}{}".format(base_url, name_list[screen_num])
+        webbrowser.get(using='google-chrome').open_new(url)
 
 
 def termination_handler(signal_received, frame):
